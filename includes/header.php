6c682f3849c34fab3eff6382611bc3bd9a2cb755
Nikhil Shisode <nikhilshisodemi6@gmail.com>
2019-05-31 17:50:52 +0530

    quick

--- includes/header.php
+++ includes/header.php
@@ -80,7 +80,7 @@
 								<li><a href="page.php?type=privacy">Privacy Policy</a></li>
 								<li><a href="page.php?type=terms">Terms of Use</a></li>
 								<li><a href="page.php?type=contact">Contact Us</a></li>
-								<li><a href="enquiry.php">Enquiry</a></li>
+		
 								<?php if($_SESSION['login'])
 {?>
 								<li>Need Help?<a href="#" data-toggle="modal" data-target="#myModal3"> / Write Us </a>  </li>
